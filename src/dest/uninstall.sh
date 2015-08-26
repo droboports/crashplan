eefd27ccf0aca687c4a233be7b9302bf205f227e
Ricardo Padilha <ricardospadilha@gmail.com>
2015-08-26 12:39:40 -0700

    Fixed bug in service.subr, removed pipefail

--- src/dest/uninstall.sh
+++ src/dest/uninstall.sh
@@ -6,7 +6,7 @@ prog_dir="$(dirname "$(realpath "${0}")")"
 name="$(basename "${prog_dir}")"
 data_dir="/mnt/DroboFS/System/${name}"
 tmp_dir="/tmp/DroboApps/${name}"
-logfile="${tmp_dir}/install.log"
+logfile="${tmp_dir}/uninstall.log"
 
 # boilerplate
 if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
@@ -14,7 +14,6 @@ exec 3>&1 4>&2 1>> "${logfile}" 2>&1
 echo "$(date +"%Y-%m-%d %H-%M-%S"):" "${0}" "${@}"
 set -o errexit  # exit on uncaught error code
 set -o nounset  # exit on unset variable
-set -o pipefail # propagate last error code on pipe
 set -o xtrace   # enable script tracing
 
 if [ -e /var/lib/crashplan ]; then
