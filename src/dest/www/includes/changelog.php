210e4b3f0d390ed887302dd075d17f15cb3deb1b
Ricardo Padilha <ricardospadilha@gmail.com>
2015-12-06 11:59:00 -0800

    Updated to Crashplan 4.5.0, improved update recovery

--- src/dest/www/includes/changelog.php
+++ src/dest/www/includes/changelog.php
@@ -1,3 +1,8 @@
+<p>Changes from 4.4.1-59:</p>
+<ol>
+  <li>Crashplan updated to version 4.5.0 (<a href="https://support.code42.com/Release_Notes/CrashPlan_For_Home/4.5.0" target="_new">full changelog</a>).</li>
+  <li>Improved update recovery.</li>
+</ol>
 <p>Changes from 4.4.1:</p>
 <ol>
   <li>Crashplan updated to version 4.4.1 build 59 (<a href="http://support.code42.com/Release_Notes/CrashPlan_For_Home" target="_new">full changelog</a>).</li>
--- src/dest/www/includes/variables.php
+++ src/dest/www/includes/variables.php
@@ -1,7 +1,7 @@
 <?php 
 $app = "crashplan";
 $appname = "Crashplan";
-$appversion = "4.4.1-59";
+$appversion = "4.5.0";
 $appsite = "https://www.code42.com/crashplan/";
 $apphelp = "https://support.code42.com/CrashPlan";
 $apppage = "https://www.crashplan.com/account/login.vtl";
